49008dbc06b5eef5f50d7d6bbe86d2d7b1734393
NanorGosdanian <nanorgosdanian@gmail.com>
2022-10-14 18:12:55 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let num1=62;
+let num2=1;
 
-// Write your code here
+
+let multiply = num1*num2;
+
+
+let random=Math.floor(Math.random() * 1000) + 1;
+ 
+
+let num3 = 40;
+let num4 = 36;
+let mod = num3 % num4;
+
+
+
+let max=Math.max(9, 2, 10, 15, 20);
